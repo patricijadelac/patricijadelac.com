181941a11aaaae8811cc6f4be1d2fc2cf5fc8c0d
Patricija Delač <patricija.delac@staffbase.com>
2026-06-20 17:28:17 +0200

    add Footer component

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,3 @@
 export const AUTHOR_NAME = 'Patricija Delač'
+export const GITHUB_URL = 'https://github.com/patricijadelac'
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/patricijadelac/'
