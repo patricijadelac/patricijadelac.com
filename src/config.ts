6d313cabc91da2d41f302235d7b0cc00c3810d0c
Patricija Delač <patricija.delac@staffbase.com>
2026-07-16 14:10:01 +0200

    remove placeholder pages, add about and certification sections to homepage

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
 export const AUTHOR_NAME = 'Patricija Delač'
 export const GITHUB_URL = 'https://github.com/patricijadelac'
-export const LINKEDIN_URL = 'https://www.linkedin.com/in/patricijadelac/'
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/patricija-delac/'
+export const CREDLY_IAAP_WAS =
+  'https://www.credly.com/badges/936c762c-4526-4e4d-9bcb-373acd127a48/public_url'
